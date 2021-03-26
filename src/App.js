528fc1911cf46c51a561f075377c27ee078921c8
Kshitinjay <kshitinjay20@gmail.com>
2021-03-26 12:24:10 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-
+import React,{useState,useEffect} from "react";
+import "./App.css";
 export default function App() {
+  const [mainData,setMainData] = useState([]);
+  const [data,setData] = useState([]);
+  const [breed,setBreed] = useState("affenpinscher");
+
+  const handleBreed = (e)=>{
+    setBreed(e.target.value);
+  }
+  const handleSubBreed = (e)=>{
+    setSubBreed(e.target.value);
+  }
+
+  useEffect(()=>{
+    fetch(`https://dog.ceo/api/breeds/list/all`)
+    .then((res)=>res.json())
+    .then((res)=>setMainData(Object.keys(res.message)));
+  },[])
+
+  useEffect(()=>{
+    fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then((res)=>res.json())
+    .then((res)=>setData(res));
+  },[breed])
+
+  console.log(mainData);
+  console.log(data);
 
   return (
     <div className="App">
-	  <h1>hello world</h1>
+	  <h1>Frontend Developer Opportunity at Healthifyme | Task Round</h1>
+    <div>
+      <div className="selectContainer">
+        <label htmlFor="breed">Enter Breed</label>
+        <select name="" id="breedSelector"  onChange={handleBreed}>
+          {mainData.map((elem,i)=>(
+            <option value={elem}>{elem}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+    <p>{breed}</p>
+    <div>
+    <img src={data.message} alt=""/>
+    </div>
     </div>
   );
 }
